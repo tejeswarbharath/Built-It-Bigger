d5314457faa05ffef370a835e4e9cc2ca697135a
Tejeswar Bharath <tejeswarbharath@gmail.com>
2016-08-28 23:15:49 +0530

    Problem with GCE

---
+++ JokeDisplay/src/main/java/com/example/tejeswar/jokedisplay/JokeActivityFragment.java
@@ -0,0 +1,38 @@
+package com.example.tejeswar.jokedisplay;
+
+/**
+ * Created by tejeswar on 8/28/2016.
+ **/
+
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+
+public class JokeActivityFragment extends Fragment
+{
+    public JokeActivityFragment()
+    {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        View root = inflater.inflate(R.layout.fragment_joke, container, false);
+        Intent intent = getActivity().getIntent();
+        String joke = intent.getStringExtra(JokeActivity.JOKE_KEY);
+        TextView jokeTextView = (TextView) root.findViewById(R.id.joke_textview);
+        if (joke != null && joke.length() != 0) {
+            jokeTextView.setText(joke);
+        }
+        return root;
+    }
+}
---
+++ joketeller2/src/main/java/com/example/MyClass.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class MyClass {
+    public String getJoke(){
+        return "This is totally a funny joke";
+    }
+}
